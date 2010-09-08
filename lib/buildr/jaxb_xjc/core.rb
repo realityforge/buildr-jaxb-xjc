9216fac75a52afb95d296f92e3d9d7ca548ecf49
Peter Donald <peter@realityforge.org>
2010-09-08 13:41:35 +1000

    Separate out a "core" file to perform main part of extension

---
+++ lib/buildr/jaxb_xjc/core.rb
@@ -0,0 +1,29 @@
+module Buildr
+  module JaxbXjc
+    class << self
+
+      def jaxb_version
+        "2.2.1"
+      end
+
+      # The specs for requirements
+      def requires
+        [
+          "javax.xml.bind:jaxb-api:jar:#{jaxb_version}",
+          "com.sun.xml.bind:jaxb-impl:jar:#{jaxb_version}",
+          "com.sun.xml.bind:jaxb-xjc:jar:#{jaxb_version}"
+        ]
+      end
+
+      # Repositories containing the requirements
+      def remote_repository
+        "http://download.java.net/maven/2"
+      end
+
+      def xjc(*args)
+        cp = Buildr.artifacts(self.requires).each(&:invoke).map(&:to_s).join(File::PATH_SEPARATOR)
+        Java::Commands.java 'com.sun.tools.xjc.XJCFacade', *(args + [{ :classpath => cp }])
+      end
+    end
+  end
+end
--- lib/buildr_jaxb_xjc.rb
+++ lib/buildr_jaxb_xjc.rb
@@ -1,6 +1,6 @@
 require 'buildr'
 require 'buildr/java'
-require 'buildr/java/ant'
 
 require 'buildr/jaxb_xjc/version'
+require 'buildr/jaxb_xjc/core'
 require 'buildr/jaxb_xjc/project_extension'
